7abe91714b21c1214c0e7f33fc9bab245b32954c
thepKz <maitanthepmrthep@gmail.com>
2025-11-03 03:29:03 +0700

    return commit and clean trashtrash

--- iframe/iframe4/main.js
+++ iframe/iframe4/main.js
@@ -1,22 +1,22 @@
 // Inspired by https://dashgo.com/
 
 const content1 = document.querySelector('.content1')
-const content2 = document.querySelector('.content2')
-const content3 = document.querySelector('.content3')
+// const content2 = document.querySelector('.content2')
+// const content3 = document.querySelector('.content3')
 const path1 = document.querySelector('.path2')
-const path2 = document.querySelector('.path3')
-const path3 = document.querySelector('.path4')
+// const path2 = document.querySelector('.path3')
+// const path3 = document.querySelector('.path4')
 const path1Length = path1.getTotalLength()
-const path2Length = path2.getTotalLength()
-const path3Length = path3.getTotalLength()
+// const path2Length = path2.getTotalLength()
+// const path3Length = path3.getTotalLength()
 path1.style.strokeDasharray  = path1Length
 path1.style.strokeDashoffset = calcDashoffset(window.innerHeight * 0.8, content1, path1Length)
 
-path2.style.strokeDasharray  = path2Length
-path2.style.strokeDashoffset = path2Length
+// path2.style.strokeDasharray  = path2Length
+// path2.style.strokeDashoffset = path2Length
 
-path3.style.strokeDasharray  = path3Length
-path3.style.strokeDashoffset = path3Length
+// path3.style.strokeDasharray  = path3Length
+// path3.style.strokeDashoffset = path3Length
 
 function calcDashoffset(scrollY, element, length) {
   const ratio = (scrollY - element.offsetTop) / element.offsetHeight
@@ -27,8 +27,8 @@ function calcDashoffset(scrollY, element, length) {
 function scrollHandler() {
   const scrollY = window.scrollY + (window.innerHeight * 0.8)
   path1.style.strokeDashoffset = calcDashoffset(scrollY, content1, path1Length)
-  path2.style.strokeDashoffset = calcDashoffset(scrollY, content2, path2Length)
-  path3.style.strokeDashoffset = calcDashoffset(scrollY, content3, path3Length)
+  // path2.style.strokeDashoffset = calcDashoffset(scrollY, content2, path2Length)
+  // path3.style.strokeDashoffset = calcDashoffset(scrollY, content3, path3Length)
 }
 
 window.addEventListener('scroll', scrollHandler)
